ac7c77dc7415e3e64768e396edb1d964b744350b
ParantezTech <parantezprojects@gmail.com>
2026-02-09 15:57:46 +0300

    added global title

--- config/app.meta.js
+++ config/app.meta.js
@@ -3,4 +3,5 @@ module.exports = {
     productName: "NapiElectronCpp",
     copyright: "Copyright © 2026 par274",
     company: "par274",
+    title: "A simple Electron app with C++ native addon"
 };
